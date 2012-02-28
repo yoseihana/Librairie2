fb88546225b4c68cb7fc5c5cbfde9c8aaa9bb167
Annabelle Buffart <anna.buffart@gmail.com>
2012-02-28 12:00:50 +0100

    Changement avec getAuthor

--- controleur/livre.php
+++ controleur/livre.php
@@ -3,121 +3,123 @@
 include 'modeles/' . $c . '.php'; //pr se connecter à la BdD
 // Produire les données à affichée et les données à utiliser. REgarde ce qu'on demande 
 
-function lister() 
-{// création de la $data et $html
+function lister()
+{ // création de la $data et $html
     global $a; // pr déclarer les variables qui sont en dehors de la fonction, elles sont globale
     global $c;
 
     $data['view_title'] = 'Liste des livres';
-    $data['livres']     = getList(); // Utilisation d'une fct dans le modèle. Utilisation de $c dedans?
-    $html              = $a . $c . '.php';
+    $data['livres'] = getList(); // Utilisation d'une fct dans le modèle. Utilisation de $c dedans?
+    include 'modeles/auteur.php';
+    $nbLivres = count($data['livres']);
+    for ($i = 0; $i < $nbLivres; $i++) {
+        $isbn = $data['livres'][$i]['isbn'];
+        $data['livres'][$i]['auteurs'] = getAuthorsForBook($isbn);
+    } //Permet d'avoir une nouvelle req pour afficher l'auteur en lui-même
+
+
+    $html = $a . $c . '.php';
     return array('data' => $data, 'html' => $html);
 }
 
-function modifier() 
+function modifier()
 {
     global $a, $c, $validActions, $validEntities;
 
-    if (isset($_REQUEST['isbn'])) 
-    { // vérifie si il y a bien qqch ds URL, tjs en GET
+    if (isset($_REQUEST['isbn'])) { // vérifie si il y a bien qqch ds URL, tjs en GET
         $isbn = $_REQUEST['isbn'];
-        if (!_isbnExiste($isbn)) 
-        {
+        if (!_isbnExiste($isbn)) {
             die('l\'isbn fournit n\'existe pas dans la base de donnée!');
             //header('Location:index.php?c=error&a=e_404');
         }
     }
-    else 
+    else
     {
         die('vous devez fournir un isbn pour voir le livre');
         //header('Location:index.php?c=error&a=e_404');
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-    {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $champsLivre['titre'] = $_POST['titre'];
         $champsLivre['nombre_page'] = $_POST['nombre_page'];
         $champsLivre['date_parution'] = $_POST['date_parution'];
         $champsLivre['genre'] = $_POST['genre'];
         $champsLivre['isbn'] = $isbn;
-        
+
         update($champsLivre);
-        
-       header('Location:'.$_SERVER['PHP_SELF'].'?a='.$validActions['voir'].'&c='.$validEntities['livre'].'&isbn='.$isbn); // donne la page index.php qui est par défaut
+
+        header('Location:' . $_SERVER['PHP_SELF'] . '?a=' . $validActions['voir'] . '&c=' . $validEntities['livre'] . '&isbn=' . $isbn); // donne la page index.php qui est par défaut
     }
-    elseif ($_SERVER['REQUEST_METHOD'] == 'GET') 
+    elseif ($_SERVER['REQUEST_METHOD'] == 'GET')
     {
-        
+
         $data['livre'] = getOne($isbn);
         $data['view_title'] = 'Modification du livre: ' . $data['livre']['titre'];
         $html = $a . $c . '.php';
-        
+
         return array('data' => $data, 'html' => $html); // returne
     }
 
-    
+
 }
 
-function ajouter() 
+function ajouter()
 {
     global $a, $c, $validActions, $validEntities;
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-    {
-        
-      add();
-        
-       header('Location:'.$_SERVER['PHP_SELF']); // donne la page index.php qui est par défaut
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        add();
+
+        header('Location:' . $_SERVER['PHP_SELF']); // donne la page index.php qui est par défaut
     }
-    elseif ($_SERVER['REQUEST_METHOD'] == 'GET') 
+    elseif ($_SERVER['REQUEST_METHOD'] == 'GET')
     {
-        
-       
+
+
         $data['view_title'] = 'Ajout de livre: ';
-        $html = $a . $c .'.php';
-        
+        $html = $a . $c . '.php';
+
         return array('data' => $data, 'html' => $html); // returne
     }
 }
 
-function supprimer() 
+function supprimer()
 {
     global $a, $c, $validActions, $validEntities;
 
-    if (isset($_REQUEST['isbn'])) 
-    {
+    if (isset($_REQUEST['isbn'])) {
         $isbn = $_REQUEST['isbn'];
-        if (!_isbnExiste($isbn)) 
-        {
+        if (!_isbnExiste($isbn)) {
             header('Location:index.php?c=error&a=e_404');
         }
     }
-    else 
+    else
     {
         header('Location:index.php?c=error&a=e_404');
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-    {
-        
-      delete($isbn);
-        
-       header('Location:'.$_SERVER['PHP_SELF']); // donne la page index.php qui est par défaut
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        delete($isbn);
+
+        header('Location:' . $_SERVER['PHP_SELF']); // donne la page index.php qui est par défaut
     }
-    elseif ($_SERVER['REQUEST_METHOD'] == 'GET') 
+    elseif ($_SERVER['REQUEST_METHOD'] == 'GET')
     {
-        
+
         $data['livre'] = getOne($isbn);
         $data['view_title'] = 'Supression du livre: ' . $data['livre']['titre'];
         $html = $a . $c . '.php';
-        
+
         return array('data' => $data, 'html' => $html); // returne
     }
 
-    
+
 }
 
-function voir() { // récupérer 1x les informations d'1 seul livre
+function voir()
+{ // récupérer 1x les informations d'1 seul livre
     global $a, $c;
 
     if (isset($_GET['isbn'])) { // vérifie si il y a bien qqch ds URL, tjs en GET
@@ -132,15 +134,15 @@ function voir() { // récupérer 1x les informations d'1 seul livre
         //header('Location:index.php?c=error&a=e_404');
     }
 
-    $data['livres']     = getList();
-    $data['livre']      = getOne($isbn);
+    $data['livres'] = getList();
+    $data['livre'] = getOne($isbn);
     $data['view_title'] = 'Fiche du livre: ' . $data['livre']['titre'];
-    $html               = $a . $c . '.php';
+    $html = $a . $c . '.php';
 
     return array('data' => $data, 'html' => $html);
 }
 
-function _isbnExiste($isbn) 
+function _isbnExiste($isbn)
 { // uniquement ds se fichier, commence par un _ car utiliser uniquement ici
     if (!getISBNCount($isbn)) { // compte le nbre d'occurance d'ISBN, il devrait y en avoir que 1 car clé primaire
         return false;
--- modeles/auteur.php
+++ modeles/auteur.php
@@ -1,60 +1,65 @@
 <?php
 
-	function getList(){ 
-            global $connex;
-            
-		$req = 'SELECT * FROM auteur ORDER BY nom ASC'; 
-		
-                try{
-		$res = $connex->query($req);
-		$auteur = $res->fetchAll();
-                }
-                catch (PDOException $e){
-                    die( $e->getMessage () );
-                }
-                return $auteur;
-	}
-	
-	function getOne($id_auteur){
-                global $connex;
-            
-		$req = 'SELECT * FROM auteur WHERE id_auteur = :id_auteur';
-		
-                try{
-		$ps = $connex->prepare($req);
-		
-		$ps->bindValue(':id_auteur', $id_auteur); 
-		$ps->execute(); 
-		
-		$auteur = $ps->fetch(); 
-                }
-                catch(PDOException $e){
-                    die($e->getMessage());
-                }
-		return $auteur;
-	
-	}
-	
-	function delete($data){
-            
-            global $connex;
-		$req = 'DELETE FROM auteur WHERE id_auteur = :id_auteur'; 
-                
-                try{
-                    $ps = $connex->prepare($req);
-                    $ps->bindValue(':id_auteur', $data['id_auteur']);
-                    $ps->execute();  
-		
-                    
-                }
-                catch(PDOException $e){
-                    die($e->getMessage());
-                }
-		
-                return true;
-	}
-        
- function update($data) {
+function getList()
+{
+    global $connex;
+
+    //$req = 'SELECT * FROM auteur ORDER BY nom ASC';
+    $req = 'SELECT livre.*, ecrit.*, auteur.*, zone.* FROM livre LEFT JOIN ecrit ON livre.isbn = ecrit.isbn LEFT JOIN auteur ON ecrit.id_auteur = auteur.id_auteur LEFT JOIN zone ON livre.code_zone = zone.code_zone WHERE livre.isbn = :isbn';
+
+    try {
+        $res = $connex->query($req);
+        $auteur = $res->fetchAll();
+    }
+    catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    return $auteur;
+}
+
+function getOne($id_auteur)
+{
+    global $connex;
+
+    $req = 'SELECT * FROM auteur WHERE id_auteur = :id_auteur';
+
+    try {
+        $ps = $connex->prepare($req);
+
+        $ps->bindValue(':id_auteur', $id_auteur);
+        $ps->execute();
+
+        $auteur = $ps->fetch();
+    }
+    catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    return $auteur;
+
+}
+
+function delete($data)
+{
+
+    global $connex;
+    $req = 'DELETE FROM auteur WHERE id_auteur = :id_auteur';
+
+    try {
+        $ps = $connex->prepare($req);
+        $ps->bindValue(':id_auteur', $data['id_auteur']);
+        $ps->execute();
+
+
+    }
+    catch (PDOException $e) {
+        die($e->getMessage());
+    }
+
+    return true;
+}
+
+function update($data)
+{
 
     global $connex;
 
@@ -66,14 +71,14 @@
         $ps->bindValue(':id_auteur', $data['id_auteur']);
         $ps->bindValue(':nom', $data['nom']);
         $ps->bindValue(':prenom', $data['prenom']);
-        
-       /* if(':date_naissance' !== '0000-00-00')
-        {*/
-       $ps -> bindValue(':date_naissance', $data['date_naissance']);
-            
-       /* }*/
 
-       
+        /* if(':date_naissance' !== '0000-00-00')
+        {*/
+        $ps->bindValue(':date_naissance', $data['date_naissance']);
+
+        /* }*/
+
+
         $ps->execute();
     }
     catch (PDOException $e) {
@@ -83,46 +88,48 @@
 
     return true;
 }
-        
-         
- function add() {
 
-    
-   if(!getidAuteurCount($_POST['id_auteur']))
-    {
+// Ajouter fct getAuthorsForBook. Utilisation de jointure, pr lier les 2 tables. Fair idem dans le blog. Utilisation de jointure
+
+function add()
+{
+
+
+    if (!getidAuteurCount($_POST['id_auteur'])) {
         global $connex;
 
-    $req = 'INSERT INTO auteur VALUES (:id_auteur, :nom, :prenom, :date_naissance);';
-   // $req2 = 'INSERT INTO ecrit VALUES (:isbn, :id_auteur)';
-    
-    
-    try {
-        $ps = $connex->prepare($req);
+        $req = 'INSERT INTO auteur VALUES (:id_auteur, :nom, :prenom, :date_naissance);';
+        // $req2 = 'INSERT INTO ecrit VALUES (:isbn, :id_auteur)';
 
-        $ps->bindValue(':id_auteur', $_POST['id_auteur']);
-        $ps->bindValue(':nom', $_POST['nom']);
-        $ps->bindValue(':prenom', $_POST['prenom']);
-        $ps->bindValue(':date_naissance', $_POST['date_naissance']);
-        $ps->execute();
-        
-        /*$ps = $connex->prepare($req2);
-        $ps->bindValue(':isbn', $_POST['isbn']);
-        $ps->bindValue(':id_auteur', $_POST['id_auteur']);
-        $ps->execute();*/
-    }
-    catch (PDOException $e) {
-        die($e->getMessage());
-        //header('Location: index.php?c=error&a=e_database');
-    }
 
-    return true;
-   }
+        try {
+            $ps = $connex->prepare($req);
+
+            $ps->bindValue(':id_auteur', $_POST['id_auteur']);
+            $ps->bindValue(':nom', $_POST['nom']);
+            $ps->bindValue(':prenom', $_POST['prenom']);
+            $ps->bindValue(':date_naissance', $_POST['date_naissance']);
+            $ps->execute();
+
+            /*$ps = $connex->prepare($req2);
+            $ps->bindValue(':isbn', $_POST['isbn']);
+            $ps->bindValue(':id_auteur', $_POST['id_auteur']);
+            $ps->execute();*/
+        }
+        catch (PDOException $e) {
+            die($e->getMessage());
+            //header('Location: index.php?c=error&a=e_database');
+        }
+
+        return true;
+    }
     else {
         return false;
-   }
+    }
 }
- 
-function getidAuteurCount($id_auteur) {
+
+function getidAuteurCount($id_auteur)
+{
     global $connex; // récupérer la connection
     $req = 'SELECT count(id_auteur) AS nb_id_auteur FROM auteur WHERE id_auteur = :id_auteur'; // récupère le nbre d'isbn
 
@@ -140,4 +147,24 @@ function getidAuteurCount($id_auteur) {
     $nbidAuteur = $nbidAuteur['nb_id_auteur']; // extraction du nbre de ISBN trouver
 
     return $nbidAuteur['nb_id_auteur']; // retourne 0 ou 1
+}
+
+function getAuthorsForBook($author)
+{
+    global $connex;
+    $req = 'SELECT livre.*, ecrit.*, auteur.*, zone.* FROM livre LEFT JOIN ecrit ON livre.isbn = ecrit.isbn LEFT JOIN auteur ON ecrit.id_auteur = auteur.id_auteur LEFT JOIN zone ON livre.code_zone = zone.code_zone WHERE livre.isbn = :isbn'; // jointure exetrne
+
+    try {
+        $ps = $connex->prepare($req);
+
+        $ps->bindValue(':isbn', $data['isbn']);
+
+        $ps->execute();
+    }
+    catch (PDOException $e) {
+        die($e->getMessage());
+        //header('Location: index.php?c=error&a=e_database');
+    }
+
+    return true;
 }
