9cccdf3089f1b8c8e8ba99cefde847053cec42dd
Alexis Vandendaele <alexis.vandendaele@gmail.com>
2012-05-26 11:23:30 +0200

    Recreated Zone

---
+++ modeles/Zone.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: alexis
+ * Date: 26/05/12
+ * Time: 11:22
+ * To change this template use File | Settings | File Templates.
+ */
+class Zone
+{
+
+}
